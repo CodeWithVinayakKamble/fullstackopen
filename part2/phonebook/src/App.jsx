f36272279068c9f0a37206a50868a80f7e676641
Vinayak Kamble <110600027+CodeWithVinayakKamble@users.noreply.github.com>
2026-08-26 21:47:05 +0530

    Exercise 2.11 from phonebook completed - Axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 const Filter = ({ value, handler }) => {
   return (
@@ -44,17 +45,21 @@ const Persons = ({ personsToShow }) => {
 const App = () => {
 
   // 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
-
+  const [persons, setPersons] = useState([]);
   // Locked html Inputs into React state here;
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
+
+  // fetching data here
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        const { data } = response;
+        setPersons(data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault();
