c199c2d55fde57af89ebe61e75cb2aa58be7253b
Vinayak Kamble <110600027+CodeWithVinayakKamble@users.noreply.github.com>
2026-09-22 02:38:06 +0530

    blog-list app setted to test environment like cross-env , supertest and configuration env and scripts

---
+++ part4/blog-list/tests/node_api.test.js
@@ -0,0 +1,8 @@
+const { test, describe, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const app = require('../app')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URL = process.env.MONGODB_URI
+const TEST_MONGODB_URI = process.env.TEST_MONGODB_URI
+
+const MONGODB_URI = process.env.NODE_ENV === "test" ? TEST_MONGODB_URI : MONGODB_URL
 const PORT = process.env.PORT || 3003
 
 module.exports = { MONGODB_URI, PORT }
